3905235388387480be976c6d8654ebe9b8811aa2
RKtheDragon <rkg07n@gmail.com>
2019-11-05 14:14:49 +0530

    added new javaProject javaFundamentals

--- day1/HelloWorld.java
+++ day1/HelloWorld.java
@@ -49,9 +49,104 @@ for (int i=0; i < 5; i++)
 
 // Print Pattern **** using for loop 
 // Place the class file authomatically in Design folder
+// javac -d ../Design pattern.java
 // Generate Documentation using JavaDoc 
 
+// git push -u origin master
 
+
+
+
+
+
+
+
+
+
+
+
+
+// *
+// **
+// ***
+public class JavaProgram
+{
+    public static void main(String args[])
+    {
+        int i, j;
+        for(i=0; i<5; i++)
+        {
+            for(j=0; j<=i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+
+
+
+
+//   *
+//  **
+// ***
+public class JavaProgram
+{
+    public static void main(String args[])
+    {
+        int i, j, k=8;
+        for(i=0; i<5; i++)
+        {
+            for(j=0; j<k; j++)
+            {
+                System.out.print(" ");
+            }
+            k = k - 2;
+            for(j=0; j<=i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+
+
+
+// Printing a pyramid
+import java.util.Scanner;
+
+public class JavaProgram
+{
+    public static void main(String args[])
+    {
+        int i, space, rows, k=0;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Number of Rows : ");
+        rows = scan.nextInt();
+        for(i=1; i<=rows; i++)
+        {
+            for(space=1; space<=(rows-i); space++)
+            {
+                System.out.print("  ");
+            }
+            while(k != (2*i-1))
+            {
+                System.out.print("* ");
+                k++;
+            }
+            k = 0;
+            System.out.println();
+        }
+    }
+}
 
 
 
